53b6c83e9d2bb79c4c6e20dd63e3930c77c78dd6
Eduardo Arroyo <aeduardoiran@gmail.com>
2024-12-06 21:16:53 -0800

    Actualizacion de errores

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import './App.css'
 import "tailwindcss/tailwind.css";
-import Acordeon from "@/components/Accordion.tsx";
 import Toast from "@/components/Toast.tsx";
 import { Toaster } from "@/components/ui/toaster"
 import Carrusel from "@/components/Carousel.tsx";
-import {DockDemo} from "@/components/Dock.tsx";
-import {ScrollImages} from "@/components/ScrollImages.tsx";
 import {MarqueeDemo} from "@/components/Scroll.tsx";
 import {Video} from "@/components/Video.tsx";
 
