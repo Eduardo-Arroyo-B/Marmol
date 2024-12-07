5b0798ae495800b9c120a98027b7693d82fe4765
Eduardo Arroyo <aeduardoiran@gmail.com>
2024-12-06 21:23:09 -0800

    Actualizacion de variables sin usar

--- src/components/Scroll.tsx
+++ src/components/Scroll.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import Marquee from "@/components/ui/marquee";
 import Imagen1 from "@/assets/SILESTONE-LOGO-big.webp"
 import Imagen2 from "@/assets/Cosentino-Logo.png"
@@ -23,42 +22,6 @@ const logos = [
         img: Imagen4
     }
 ]
-
-const ReviewCard = ({
-                        img,
-                        name,
-                        username,
-                        body,
-                    }: {
-    img: string;
-    name: string;
-    username: string;
-    body: string;
-}) => {
-    return (
-        <figure
-            className={cn(
-                "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-                // light styles
-                "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-                // dark styles
-                "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-            )}
-        >
-            <div className="flex flex-row items-center gap-2">
-                <img className="rounded-full" width="32" height="32" alt="" src={img} />
-                <div className="flex flex-col">
-                    <figcaption className="text-sm font-medium dark:text-white">
-                        {name}
-                    </figcaption>
-                    <p className="text-xs font-medium dark:text-white/40">{username}</p>
-                </div>
-            </div>
-            <blockquote className="mt-2 text-sm">{body}</blockquote>
-        </figure>
-    );
-};
-
 export function MarqueeDemo() {
     return (
         <div className="relative flex h-[200px] w-[700px] flex-col items-center justify-center overflow-hidden">
